2701e43e0d2ffaf461e4d8601c6b2dd4fcebb8ab
KRI$H <divya.aero99@gmail.com>
2022-08-03 06:40:22 +0530

    HELLO

---
+++ src/org/prac/Practice.java
@@ -0,0 +1,13 @@
+package org.prac;
+
+public class Practice {
+
+	private void empId() {
+
+		System.out.println("empid is 1234");
+	}
+	public static void main(String[] args) {
+		 Practice practice=new Practice();
+		 practice.empId();
+	}
+}
