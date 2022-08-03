ec5fc28c462c957bb7f9f193a0c86c8ff21aac43
KRI$H <divya.aero99@gmail.com>
2022-08-03 13:48:01 +0530

    WELCOME

---
+++ src/org/prac/New.java
@@ -0,0 +1,13 @@
+package org.prac;
+
+public class New {
+	
+	private void empName() {
+    System.out.println("empName is DIVYA");
+	}
+	public static void main(String[] args) {
+		New new1=new New();
+		new1.empName();
+	}
+
+}
